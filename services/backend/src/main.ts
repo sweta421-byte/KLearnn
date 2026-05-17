32fe4e508f99897c2d050be14c0200359f0ab70b
sweta421 <sweta88232@gmail.com>
2026-05-17 19:09:19 +0530

    fix: cors and port for production

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://k-learnn.vercel.app'],
     credentials: true,
   });
-
-  await app.listen(3001);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
